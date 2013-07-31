bde8704e900b18a56f06fcbb479d1426019666d7
Delta <d@qiscus.com>
2013-07-31 13:22:45 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = '67ac47876197770c3ba6c7001b39257c59d38c8c20101e7b0a1d2fb42cc7b54a1251cb8adeaa1df2dfdf1351355347d7b9b6028b3fc7773e7336d7e7cd8eacf0'
